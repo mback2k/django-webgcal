562241081cf9436b350ad26ead521704e0a58fb3
Marc Hoersken <info@marc-hoersken.de>
2010-12-21 21:24:33 +0100

    Updated views

--- webgcal/views.py
+++ webgcal/views.py
@@ -49,16 +49,14 @@ def create_item(request):
 
 @login_required
 def show_item(request, id):
-    services = Service.objects.all().filter(user=request.user).order_by('-tstamp')
-    service = get_object_or_404(Service, user=request.user, id=id)
-
-    Result.objects.all().filter(crdate__lt=datetime.datetime.now()-datetime.timedelta(days=7)).delete()
+    calendars = Calendar.objects.all().filter(user=request.user).order_by('-tstamp')
+    calendar = get_object_or_404(Calendar, user=request.user, id=id)
 
     template_values = {
-        'services': services,
+        'calendars': calendars,
         'create_form': None,
         'edit_form': None,
-        'service': service,
+        'calendar': calendar,
         'message': None,
     }
     
@@ -66,82 +64,40 @@ def show_item(request, id):
 
 @login_required
 def edit_item(request, id):
-    services = Service.objects.all().filter(user=request.user).order_by('-tstamp')
-    service = get_object_or_404(Service, user=request.user, id=id)
-    edit_form = ServiceForm(instance=service, data=request.POST if request.method == 'POST' else None)
-    edit_form.id = service.id
+    calendars = Calendar.objects.all().filter(user=request.user).order_by('-tstamp')
+    calendar = get_object_or_404(Calendar, user=request.user, id=id)
+    edit_form = CalendarForm(instance=calendar, data=request.POST if request.method == 'POST' else None)
+    edit_form.id = calendar.id
     
     if edit_form.is_valid():
-        service = edit_form.save(commit=False)
-        service.user = request.user
-        service.waiting = True
-        service.save()
+        calendar = edit_form.save(commit=False)
+        calendar.user = request.user
+        calendar.save()
         edit_form = None
     
     template_values = {
-        'services': services,
+        'calendars': calendars,
         'create_form': None,
         'edit_form': edit_form,
-        'service': service,
+        'calendar': calendar,
         'message': None,
     }
     
     return render_to_response('show_dashboard.html', template_values, context_instance=RequestContext(request))
 
 @login_required
-def switch_item(request, id):
-    services = Service.objects.all().filter(user=request.user).order_by('-tstamp')
-    service = get_object_or_404(Service, user=request.user, id=id)
-    service.enabled = not(service.enabled)
-    service.save()
-    create_form = ServiceForm()
-    message = 'Switched service %s!' % ('on' if service.enabled else 'off')
-
-    template_values = {
-        'services': services,
-        'create_form': create_form,
-        'edit_form': None,
-        'service': None,
-        'message': message,
-    }
-
-    return render_to_response('show_dashboard.html', template_values, context_instance=RequestContext(request))
-
-@login_required
-def force_item(request, id):
-    services = Service.objects.all().filter(user=request.user).order_by('-tstamp')
-    service = get_object_or_404(Service, user=request.user, id=id)
-    service.waiting = True
-    service.save()
-    message = 'The service will be updated on next IP check!'
-
-    template_values = {
-        'services': services,
-        'create_form': None,
-        'edit_form': None,
-        'service': service,
-        'message': message,
-    }
-
-    return render_to_response('show_dashboard.html', template_values, context_instance=RequestContext(request))
-
-def feed_item(request, id):
-    feed = ResultFeed()
-    return feed(request, id)
-
-@login_required
 def delete_item(request, id):
-    services = Service.objects.all().filter(user=request.user).order_by('-tstamp')
-    service = get_object_or_404(Service, user=request.user, id=id)
-    service.delete()
-    create_form = ServiceForm()
-    message = 'Deleted service from your Dashboard!'
+    calendars = Calendar.objects.all().filter(user=request.user).order_by('-tstamp')
+    calendar = get_object_or_404(Calendar, user=request.user, id=id)
+    calendar.delete()
+    create_form = CalendarForm()
+    message = 'Deleted calendar from your Dashboard!'
 
     template_values = {
-        'services': services,
+        'calendars': services,
         'create_form': create_form,
         'edit_form': None,
-        'service': None,
+        'calendar': None,
         'message': message,
     }
 
@@ -149,21 +105,17 @@ def delete_item(request, id):
 
 @login_required
 def delete_item_ask(request, id):
-    services = Service.objects.all().filter(user=request.user).order_by('-tstamp')
-    service = get_object_or_404(Service, user=request.user, id=id)
-    create_form = ServiceForm()
-    message = 'Do you want to delete %s? <a href="%s" title="Yes">Yes</a>' % (service, reverse('webgcal.views.delete_item', kwargs={'id': id}))
+    calendars = Calendar.objects.all().filter(user=request.user).order_by('-tstamp')
+    calendar = get_object_or_404(Calendar, user=request.user, id=id)
+    create_form = CalendarForm()
+    message = 'Do you want to delete %s? <a href="%s" title="Yes">Yes</a>' % (calendar, reverse('webgcal.views.delete_item', kwargs={'id': id}))
 
     template_values = {
-        'services': services,
+        'calendars': calendars,
         'create_form': create_form,
         'edit_form': None,
-        'service': None,
+        'calendar': None,
         'message': message,
     }
 
     return render_to_response('show_dashboard.html', template_values, context_instance=RequestContext(request))
-
-def feed_item_key(request, key):
-    from google.appengine.ext import db
-    return HttpResponseRedirect(reverse('webgcal.views.feed_item', kwargs={'id': db.Key(key).id()}))
