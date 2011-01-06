859c3c8ba1aac90d6b45b98ae88b9c65dc2d38ce
Marc Hoersken <info@marc-hoersken.de>
2011-01-06 22:33:04 +0100

    Added django helper bats

---
+++ django_setup.bat
@@ -0,0 +1,4 @@
+mklink /D "dbindexer" "D:\Web\django-dbindexer\dbindexer"
+mklink /D "django" "D:\Web\django-nonrel\django"
+mklink /D "djangoappengine" "D:\Web\djangoappengine"
+mklink /D "djangotoolbox" "D:\Web\djangotoolbox\djangotoolbox"
---
+++ django_update.bat
@@ -0,0 +1,17 @@
+cd ../
+
+cd django-dbindexer
+hg pull -u
+cd ../
+
+cd django-nonrel
+hg pull -u
+cd ../
+
+cd djangoappengine
+hg pull -u
+cd ../
+
+cd djangotoolbox
+hg pull -u
+cd ../
