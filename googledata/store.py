481ab57b69bcc124113e8531015e60b738ba352c
Marc Hoersken <info@marc-hoersken.de>
2011-11-05 22:09:35 +0100

    Fixed tokens not being automatically stored

--- googledata/store.py
+++ googledata/store.py
@@ -4,7 +4,7 @@ import atom.http_interface
 import atom.token_store
 import pickle
 
-def run_on_django(gdata_service, request=None, deadline=10):
+def run_on_django(gdata_service, request=None, store_tokens=True, single_user_mode=False, deadline=10):
     try:
         import google.appengine
     except:
@@ -12,11 +12,16 @@ def run_on_django(gdata_service, request=None, deadline=10):
     else:
         from gdata.alt.appengine import run_on_appengine
         run_on_appengine(gdata_service, deadline=deadline)
+
     try:
         gdata_service._SetSessionId(None)
     except:
         pass
+
     gdata_service.token_store = DjangoTokenStore(request)
+    gdata_service.auto_store_tokens = store_tokens
+    gdata_service.auto_set_current_token = single_user_mode
+
     return gdata_service
 
 
