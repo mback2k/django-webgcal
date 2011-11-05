24c1ec1dd2a057199482e31cf2e6f4d91b946f74
Marc Hoersken <info@marc-hoersken.de>
2011-11-05 19:52:40 +0100

    Updated googleauth to work on plain django

--- googleauth/urls.py
+++ googleauth/urls.py
@@ -1,33 +1,13 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.contrib.auth import views, authenticate, login, logout
+from django.contrib.auth import views
 from django.contrib.admin import sites
 
-def view_login(request):
-    from google.appengine.api import users
-    from django.core.urlresolvers import reverse
-    from django.http import HttpResponseRedirect
-    from django.conf import settings
-    
-    user = authenticate()
-    if user:
-        login(request, user)
-        return HttpResponseRedirect(getattr(settings, 'LOGIN_REDIRECT_URL', '/'))
-    
-    return HttpResponseRedirect(users.create_login_url(reverse('django.contrib.auth.views.login')))
-    
-def view_logout(request):
-    from google.appengine.api import users
-    from django.http import HttpResponseRedirect
-    from django.conf import settings
-    
-    logout(request)
-    
-    return HttpResponseRedirect(users.create_logout_url(getattr(settings, 'LOGOUT_REDIRECT_URL', '/')))
+import views as v
 
-views.login = view_login
-views.logout = view_logout
-sites.AdminSite.login = lambda s, r: login(r)
+views.login = v.view_login
+views.logout = v.view_logout
+sites.AdminSite.login = lambda s, r: v.view_login(r)
 
 urlpatterns = patterns('django.contrib.auth.views',
     (r'^%s$' % getattr(settings, 'LOGIN_URL', '/login/')[1:], 'login'),
