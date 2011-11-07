58fc2956366d70d83f3e74e04a738c722e7b582d
Marc Hoersken <info@marc-hoersken.de>
2011-11-07 16:47:54 +0100

    Moved run_on_django function to module

--- googledata/__init__.py
+++ googledata/__init__.py
@@ -1,1 +1,21 @@
-from store import run_on_django
+from store import DjangoTokenStore
+
+def run_on_django(gdata_service, request=None, store_tokens=True, single_user_mode=False, deadline=10):
+    try:
+        import google.appengine
+    except:
+        pass
+    else:
+        from gdata.alt.appengine import run_on_appengine
+        gdata_service = run_on_appengine(gdata_service, deadline=deadline)
+
+    try:
+        gdata_service._SetSessionId(None)
+    except:
+        pass
+
+    gdata_service.token_store = DjangoTokenStore(request)
+    gdata_service.auto_store_tokens = store_tokens
+    gdata_service.auto_set_current_token = single_user_mode
+
+    return gdata_service
--- googledata/store.py
+++ googledata/store.py
@@ -5,27 +5,6 @@ import atom.token_store
 import base64
 import pickle
 
-def run_on_django(gdata_service, request=None, store_tokens=True, single_user_mode=False, deadline=10):
-    try:
-        import google.appengine
-    except:
-        pass
-    else:
-        from gdata.alt.appengine import run_on_appengine
-        gdata_service = run_on_appengine(gdata_service, deadline=deadline)
-
-    try:
-        gdata_service._SetSessionId(None)
-    except:
-        pass
-
-    gdata_service.token_store = DjangoTokenStore(request)
-    gdata_service.auto_store_tokens = store_tokens
-    gdata_service.auto_set_current_token = single_user_mode
-
-    return gdata_service
-
-
 class DjangoTokenStore(atom.token_store.TokenStore):
     def __init__(self, request):
         self.user = request.user if request else None
