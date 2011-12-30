c47b0fdeec18fc726ea38679c267d8c3627757ae
Marc Hoersken <info@marc-hoersken.de>
2011-12-31 00:27:36 +0100

    Moved googledata into submodule

--- googledata/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class TokenCollection(models.Model):
-    user = models.ForeignKey(User)
-    pickled_tokens = models.TextField(db_index=False)
