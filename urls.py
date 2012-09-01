c2542b72b7b709fab9c75ef551e47bf440b714d6
Marc Hoersken <info@marc-hoersken.de>
2012-09-02 00:07:00 +0200

    Moved webgcal urls to the top

--- urls.py
+++ urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'', include('webgcal.urls')),
     url(r'', include('django.contrib.auth.urls')),
     url(r'', include('social_auth.urls')),
-    url(r'', include('webgcal.urls')),
 )
