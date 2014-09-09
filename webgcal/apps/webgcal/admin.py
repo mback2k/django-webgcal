af4c58e4ca37b8981e4de51d4d3ac329a240fe96
Marc Hoersken <info@marc-hoersken.de>
2014-09-09 23:11:26 +0200

    Enabled search for some admin modules

--- webgcal/apps/webgcal/admin.py
+++ webgcal/apps/webgcal/admin.py
@@ -5,11 +5,13 @@ from .models import Calendar, Website, Event
 class CalendarAdmin(admin.ModelAdmin):
     fields = ('name', 'user', 'google_id')
     list_display = ('name', 'user', 'updated', 'status', 'has_running_task', 'has_ready_task', 'enabled')
+    search_fields = ('name',)
     ordering = ('name',)
 
 class WebsiteAdmin(admin.ModelAdmin):
     fields = ('name', 'calendar', 'href')
     list_display = ('name', 'calendar', 'updated', 'status', 'has_running_task', 'has_ready_task', 'enabled')
+    search_fields = ('name',)
     ordering = ('name',)
 
 class EventAdmin(admin.ModelAdmin):
@@ -24,6 +26,7 @@ class EventAdmin(admin.ModelAdmin):
         }),
     )
     list_display = ('summary', 'website', 'parsed', 'synced')
+    search_fields = ('uid', 'summary', 'description', 'location', 'category', 'status')
 
 admin.site.register(Calendar, CalendarAdmin)
 admin.site.register(Website, WebsiteAdmin)
--- webgcal/libs/keeperr/admin.py
+++ webgcal/libs/keeperr/admin.py
@@ -4,6 +4,7 @@ from .models import Error
 
 class ErrorAdmin(admin.ModelAdmin):
     list_display = ('exc_type', 'exc_value', 'exc_timestamp', 'content_object')
+    search_fields = ('exc_type', 'exc_value')
     ordering = ('-exc_timestamp',)
 
 admin.site.register(Error, ErrorAdmin)
