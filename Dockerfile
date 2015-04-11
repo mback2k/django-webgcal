e7d26b0e332b63c64df7761b60baf2af50acfddc
Marc Hoersken <info@marc-hoersken.de>
2015-04-11 11:51:00 +0200

    Added Dockerfile for local dev and test environment

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM python:2-wheezy
+
+MAINTAINER Marc Hoersken "info@marc-hoersken.de"
+
+RUN apt-get update
+RUN apt-get install -y python-dev python-setuptools python-mysqldb
+RUN apt-get clean
+
+RUN useradd --user-group --home /usr/src django
+RUN chown django:django -R /usr/src
+
+USER django
+RUN mkdir -p /usr/src/env
+RUN virtualenv /usr/src/env
+
+RUN /usr/src/env/bin/pip install mysql-python
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY requirements.txt /usr/src/app/
+RUN /usr/src/env/bin/pip install -r requirements.txt
+
+ADD . /usr/src/app
+
+USER root
+RUN chown django:django -R /usr/src
+
+USER django
+RUN /usr/src/env/bin/python -m compileall /usr/src/app
+
+ENV DJANGO_SETTINGS_MODULE=webgcal.settings.docker
+RUN /usr/src/env/bin/python manage.py collectstatic --noinput
+RUN /usr/src/env/bin/python manage.py compress --force
+
+EXPOSE 8000
+CMD /usr/src/env/bin/python manage.py runserver 0.0.0.0:8000
---
+++ webgcal/settings/docker.py
@@ -0,0 +1,42 @@
+from .common import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'docker',                       # Or path to database file if using sqlite3.
+        'USER': 'docker',                       # Not used with sqlite3.
+        'PASSWORD': 'docker',                   # Not used with sqlite3.
+        'HOST': 'db',                           # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                         # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=InnoDB',
+        }
+    }
+}
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+BROKER_URL = 'redis://redis:6379/1'
+CELERY_RESULT_BACKEND = BROKER_URL
+
+SWAMP_DRAGON_REDIS_HOST = 'redis'
+DRAGON_URL = 'http://dockerhost:9999/'
+
+ALLOWED_HOSTS = ['dockerhost']
+DEFAULT_FROM_EMAIL = 'no-reply@dockerhost'
+
+SESSION_COOKIE_NAME = 'webgcal'
+SESSION_COOKIE_DOMAIN = 'dockerhost'
+SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_HTTPONLY = True
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'THIS_IS_A_DEVELOPMENT_KEY_WHICH_SHOULD_NOT_BE_USED_IN_PRODUCTION!'
