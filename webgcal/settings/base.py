8df44a18df90840984cb4b4e3fc243ba69408b7c
Marc Hoersken <info@marc-hoersken.de>
2016-03-11 22:02:48 +0100

    Update middleware classes to Django 1.8 defaults

--- webgcal/settings/base.py
+++ webgcal/settings/base.py
@@ -32,11 +32,14 @@ COMPRESS_JS_FILTERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'social.apps.django_app.middleware.SocialAuthExceptionMiddleware',
 )
 
