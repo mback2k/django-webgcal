c71ae64be706c3f189566ecc885abfdebd271db5
Marc Hoersken <info@marc-hoersken.de>
2019-01-20 16:07:45 +0100

    Fix timezone default value to be a unicode string

--- webgcal/apps/webgcal/migrations/0001_initial.py
+++ webgcal/apps/webgcal/migrations/0001_initial.py
@@ -58,7 +58,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=100, verbose_name='Name')),
                 ('href', models.URLField(verbose_name='Link')),
-                ('timezone', models.CharField(default=b'UTC', max_length=50, verbose_name='Timezone')),
+                ('timezone', models.CharField(default='UTC', max_length=50, verbose_name='Timezone')),
                 ('updated', models.DateTimeField(null=True, verbose_name='Date updated', blank=True)),
                 ('enabled', models.BooleanField(default=True, verbose_name='Enabled')),
                 ('status', models.TextField(null=True, verbose_name='Status', blank=True)),
